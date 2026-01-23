6c94e2770504f179c761a387630ac0151f0d117f
student-nithin <ninsai2007@gmail.com>
2026-01-23 23:37:26 +0530

    add new project

---
+++ Mini_project/src/calculator/compound_interest_calculator.java
@@ -0,0 +1,32 @@
+package calculator;
+
+import java.util.Scanner;
+public class compound_interest_calculator {
+    public static void main(String[] args){
+        // compound interest calculator
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        double principal;
+        double interest;
+        int timesCompounded;
+        int  years;
+        double amount;
+
+        System.out.print("Enter the  principal amount : ");
+        principal = scanner.nextDouble();
+        System.out.print("Enter  the interest : ");
+        interest  = scanner.nextDouble()/100;
+        System.out.print("Enter the number of times compounded (per year): ");
+        timesCompounded = scanner.nextInt();
+        System.out.print("Enter the number of years : ");
+        years = scanner.nextInt();
+        amount = principal * Math.pow(1 + interest / timesCompounded, timesCompounded * years);
+
+        System.out.printf("The amount after %d years is %.2f",years,amount);
+
+
+        scanner.close();
+    }
+}
