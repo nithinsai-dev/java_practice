68988e8927c648262e5a90f579753baeb6c7420a
student-nithin <ninsai2007@gmail.com>
2026-02-02 23:05:19 +0530

    New project

--- practice excercise/Practical applications/src/ElectrictybillingSystem.java
+++ practice excercise/Practical applications/src/ElectrictybillingSystem.java
@@ -1,2 +1,45 @@
+
+import java.util.Scanner;
+
 public class ElectrictybillingSystem {
+    public static void main(String[] args) {
+        int base = 10;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the units : ");
+        int units = scanner.nextInt();
+
+        double amount = 10;
+        boolean payment;
+
+        if (units < 0) {
+            System.out.println("Invalid input");
+            payment = false;
+        }
+        else {
+            System.out.print("Payment late (true/false) : ");
+            payment = scanner.nextBoolean();
+
+            if (units <= 100) {
+                amount += units * 0.5;
+            }
+            if (units > 100 && units <= 300) {
+                amount += 100 * 0.5;
+                amount += (units - 100) * 0.75 ;
+            }
+            if (units > 300) {
+                amount += 100 * 0.5;
+                amount += 200 * 0.75;
+                amount += (units - 300) * 1.2 ;
+            }
+
+            if (payment) {
+                amount += 0.05 * amount ;
+            }
+            System.out.printf("%.2f" , amount);
+        }
+
+    }
+
 }
