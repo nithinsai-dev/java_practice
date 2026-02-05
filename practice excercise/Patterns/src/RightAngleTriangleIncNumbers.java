e1fd8ef4a807db725fa44e740443f2084606d519
student-nithin <ninsai2007@gmail.com>
2026-02-05 14:20:37 +0530

    New project

---
+++ practice excercise/Patterns/src/RightAngleTriangleIncNumbers.java
@@ -0,0 +1,13 @@
+public class RightAngleTriangleIncNumbers {
+    public static void main(String[] args){
+        int n=4;
+        int x = 1;
+        for (int i=1;i<=n;i++){
+            for (int j=1;j<=i;j++){
+                System.out.print(x + " ");
+                x++;
+            }
+            System.out.println();
+        }
+    }
+}
